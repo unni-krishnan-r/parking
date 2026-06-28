5f5de3ee91e5e1cd33d104eaf432b8a8ee522492
unni-krishnan-r <unninairr25@gmail.com>
2026-06-28 21:17:11 +0530

    duplicated

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = uri
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parkeasy.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parkeasy.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'parkeasy_secret_key'
 
